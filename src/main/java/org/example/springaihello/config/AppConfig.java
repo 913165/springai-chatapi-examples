7b9c30884ca6334917161b2ae48fae2c23749dd0
Pritesh <tinumistry@gmail.com>
2024-10-02 01:35:29 +0530

    updated config file

--- src/main/java/org/example/springaihello/config/AppConfig.java
+++ src/main/java/org/example/springaihello/config/AppConfig.java
@@ -6,16 +6,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-    @Configuration
-    class Config {
 
-        @Bean
-        ChatClient chatClient(ChatClient.Builder builder) {
-            return builder.defaultSystem("You are an AI assistant that specializes in {subject}. " +
-                            "You respond in a {tone} voice with detailed explanations.")
-                    .build();
-        }
-
+    @Bean
+    ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.defaultSystem("You are an AI assistant that specializes in {subject}. " +
+                        "You respond in a {tone} voice with detailed explanations.")
+                .build();
     }
 
 }
